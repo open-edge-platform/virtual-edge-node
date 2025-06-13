666f4bb6c596578469806434ac291cb9df9bc5fa
raphaelvrosa <raphael.rosa@intel.com>
2025-06-13 16:58:29 +0200

    Fixes uuid filter in api nbi implementation

--- edge-node-simulator/pkg/en/onboard/north.go
+++ edge-node-simulator/pkg/en/onboard/north.go
@@ -378,7 +378,7 @@ func HTTPInfraOnboardGetHostID(ctx context.Context, url, token string, client *h
 		hostID = *(ps.Hosts)[0].ResourceId
 		return nil
 	}
-	if err := httpGet(rCtx, client, fmt.Sprintf("%s?uuid=%s", url, uuid), token, responseHooker); err != nil {
+	if err := httpGet(rCtx, client, fmt.Sprintf("%s?filter=uuid=%s", url, uuid), token, responseHooker); err != nil {
 		return hostID, err
 	}
 
